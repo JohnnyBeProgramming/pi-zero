614937f183c2e134965bd6c80c1bd1a07649707e
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-02-09 17:51:23 +0100

    Add debug script

---
+++ boot/debug.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# --------------------------------------------------------------------
+# Simple debug script to check service status
+# --------------------------------------------------------------------
+APP_NAME="opsec"
+APP_HOME="$HOME/app"
+
+# List all system services
+systemctl --type=service
+
+# Show boot logs
+journalctl -b
+
+# Show the service logs
+journalctl -u "$APP_NAME.service"
--- install.sh
+++ install.sh
@@ -285,24 +285,24 @@ create-startup-script() {
     if [ ! -f /etc/systemd/system/$APP_NAME.service ]; then
         echo "Injecting 'opsec' startup script..."
         cat <<- EOF | sudo tee /etc/systemd/system/$APP_NAME.service > /dev/null
-                [Unit]
-                Description=OpSec Startup Service
-                #After=systemd-modules-load.service
-                After=local-fs.target
-                DefaultDependencies=no
-                Before=sysinit.target
+[Unit]
+Description=OpSec Startup Service
+#After=systemd-modules-load.service
+After=local-fs.target
+DefaultDependencies=no
+Before=sysinit.target
 
-                [Service]
-                #Type=oneshot
-                Type=forking
-                RemainAfterExit=yes
-                ExecStart=/bin/bash $APP_HOME/app/boot_app
-                StandardOutput=journal+console
-                StandardError=journal+console
+[Service]
+#Type=oneshot
+Type=forking
+RemainAfterExit=yes
+ExecStart=/bin/bash $APP_HOME/boot/boot_simple
+StandardOutput=journal+console
+StandardError=journal+console
 
-                [Install]
-                WantedBy=multi-user.target
-                #WantedBy=sysinit.target
+[Install]
+WantedBy=multi-user.target
+#WantedBy=sysinit.target
 EOF
 fi
 
