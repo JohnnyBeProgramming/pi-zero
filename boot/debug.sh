50dd040c7173eddedc9ee0d4c31ffddf2e84cdc3
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-02-09 18:08:36 +0100

    Improve debug script

--- boot/debug.sh
+++ boot/debug.sh
@@ -5,11 +5,22 @@
 APP_NAME="opsec"
 APP_HOME="$HOME/app"
 
-# List all system services
-systemctl --type=service
 
-# Show boot logs
-#journalctl -b
-
-# Show the service logs
-journalctl -u "$APP_NAME.service"
+case $1 in
+    all) 
+		# List all system services
+		systemctl --type=service
+	;;
+    logs) 
+		# Show the service logs
+		journalctl -b		
+	;;
+    start|stop|restart) 
+		# Start, stop or restart the service
+		sudo systemctl $1 $APP_NAME.service
+	;;
+	*) 
+		# Default: Show the service logs
+		journalctl -u "$APP_NAME.service"
+	;;
+esac
