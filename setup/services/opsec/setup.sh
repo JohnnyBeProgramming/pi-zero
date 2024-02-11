570ebdf33c424343585b19f00702fe7da5015168
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-02-11 16:14:49 +0100

    ...

--- setup/services/opsec/setup.sh
+++ setup/services/opsec/setup.sh
@@ -77,7 +77,7 @@ install-service() {
     # Install the service manifest in systemd
     if [ -f "$dest/service.cfg" ]; then
         echo "Injecting '$SETUP_NAME' startup script..."
-        cat "$dest/service.cfg" > /etc/systemd/system/$SETUP_NAME.service 
+        cat "$dest/service.cfg" | sudo tee /etc/systemd/system/$SETUP_NAME.service > /dev/null
     else
         echo "Warning: Not found '$dest/service.cfg', skipping..."
         return 0
