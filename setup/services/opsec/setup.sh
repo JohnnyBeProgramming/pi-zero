1e8a95b8322594ce1039bd106766989beb3850f3
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-02-11 19:49:01 +0100

    Tweaks to setup script

--- setup/services/opsec/setup.sh
+++ setup/services/opsec/setup.sh
@@ -5,8 +5,8 @@ set -euo pipefail # Stop running the script on first error...
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 setup() {
-    SETUP_NAME=${1:-"$(basename $THIS_DIR)"}
-    SETUP_PATH=${2:-$THIS_DIR}
+    export SETUP_NAME=${1:-"$(basename $THIS_DIR)"}
+    export SETUP_PATH=${2:-$THIS_DIR}
 
     # Prepare the application folder where the service will be installed
     if [ -z "$APP_HOME" ]; then
