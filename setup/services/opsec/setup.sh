3cfbfa0bc70a70f83662a556b552a0e074be7254
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-02-11 16:11:05 +0100

    ...

--- setup/services/opsec/setup.sh
+++ setup/services/opsec/setup.sh
@@ -66,8 +66,8 @@ install-service() {
 
     echo "Updating service: $SETUP_NAME ..."
     mkdir -p "$dest"
-    echo cp -rf "$SETUP_PATH" "$dest"
-    cp -rf "$SETUP_PATH" "$dest"
+    echo rsync -a "$SETUP_PATH/" "$dest"
+    rsync -a "$SETUP_PATH/" "$dest"
     
     # Generate manifest from template if provided
     if [ -f "$dest/service.cfg.tpl" ]; then
