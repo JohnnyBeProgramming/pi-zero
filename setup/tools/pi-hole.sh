15428b446a31e62d58d11e60e2b2c55aa3bb6c85
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2025-03-02 16:40:18 +0100

    ...

---
+++ setup/tools/pi-hole.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------
+set -euo pipefail # Stop running the script on first error...
+# --------------------------------------------------------------
+
+curl -sSL https://install.pi-hole.net | bash
